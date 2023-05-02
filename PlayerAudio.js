c5db6c13b767447d2644b0f7c5075229846ab8f4
bastien-page <page.bastien.pro@gmail.com>
2023-05-02 22:28:37 +0200

    feat: mute unmute

--- PlayerAudio.js
+++ PlayerAudio.js
@@ -7,6 +7,8 @@ class PlayerAudio {
         this.track = elt.querySelector('[data-player-track]')
         this.buttonPlay = elt.querySelector('[data-player-play]')
         this.buttonPause = elt.querySelector('[data-player-pause]')
+        this.buttonMute = elt.querySelector('[data-player-mute]')
+        this.buttonUnmute = elt.querySelector('[data-player-unmute]')
     }
 
     eventHandler() {
@@ -17,12 +19,24 @@ class PlayerAudio {
             this.pause()
         })
 
+        this.buttonMute.addEventListener('click', () => {
+            this.mute(0)
+        })
+
+        this.buttonUnmute.addEventListener('click', () => {
+            this.unmute(1)
+        })
+
         this.track.addEventListener('input', (e) => {
             this.changeCurrentTime(e.target.value)
         })
 
         this.volume.addEventListener('input', (e) => {
-            this.nativePlayer.volume = e.target.value
+            if (e.target.value > 0) {
+                this.unmute(e.target.value)
+            } else {
+                this.mute(0)
+            }
         })
 
         this.nativePlayer.addEventListener('timeupdate', () => {
@@ -48,14 +62,35 @@ class PlayerAudio {
 
     play() {
         this.nativePlayer.play()
+        this.buttonPause.classList.remove('is-inactive')
+        this.buttonPlay.classList.add('is-inactive')
     }
 
     pause() {
         this.nativePlayer.pause()
+        this.buttonPause.classList.add('is-inactive')
+        this.buttonPlay.classList.remove('is-inactive')
     }
 
     changeCurrentTime(time) {
         this.nativePlayer.currentTime = time
+    }
+
+    mute(value) {
+        this.nativePlayer.muted = true
+        this.volume.value = value
+        this.nativePlayer.volume = value
+        this.buttonUnmute.classList.remove('is-inactive')
+        this.buttonMute.classList.add('is-inactive')
+    }
+
+    unmute(value) {
+        this.nativePlayer.muted = false
+        this.volume.value = value
+        console.log(this.volume.value)
+        this.nativePlayer.volume = value
+        this.buttonUnmute.classList.add('is-inactive')
+        this.buttonMute.classList.remove('is-inactive')
     }
 
     convertTime(time) {
